19aa3bca2ec9c9cd6b873aeec822607dee6fea6a
alc232 <allanchalmers22@googlemail.com>
2021-03-29 21:53:21 +0000

    adding code fixes from jshint

--- assets/game.js
+++ assets/game.js
@@ -1,151 +1,3 @@
-// let questions = [
-//     {
-//         "color": "orange",
-//         "character": "Goku",
-//         "power": "strength",
-//         "cape": "no",
-//         "weather": "sunny",
-//         "money": "yes",
-//         "image": "https://i.pinimg.com/originals/ac/a4/6d/aca46d09a1badb457d44d039f2c4754d.jpg",
-//         "counter": 0
-
-//     },
-//     {
-
-//         "color": "red",
-//         "character": "Superman",
-//         "power": "Man if Steel",
-//         "cape": "yes",
-//         "weather": "sunny",
-//         "money": "no",
-//         "image": "https://wallpaperaccess.com/full/1897373.jpg",
-//         "counter": 0
-//     },
-//     {
-//         "color": "black",
-//         "character": "Batman",
-//         "power": "money",
-//         "cape": "yes",
-//         "weather": "rain",
-//         "money": "yes",
-//         "image": "https://img.cinemablend.com/filter:scale/quill/6/b/0/8/9/2/6b08928dc1fb6884e189c7434125ba7f344407b7.jpg?mw=600",
-//         "counter": 0
-//     },
-//     {
-//         "color": "green",
-//         "character": "Green Lantern",
-//         "power": "ring",
-//         "cape": "no",
-//         "weather": "wind",
-//         "money": "yes",
-//         "image": "https://www.goodcomicstoread.com/wp-content/uploads/2020/01/green-lantern-678x381.jpg",
-//         "counter": 0
-//     },
-
-//     {
-//         "color": "silver",
-//         "character": "Thor",
-//         "power": "hammer",
-//         "cape": "yes",
-//         "weather": "wind",
-//         "money": "yes",
-//         "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomicvine.gamespot.com%2Fthor%2F4005-2268%2Fforums%2Fwould-you-watch-a-mighty-thor-cartoon-376272%2F&psig=AOvVaw3vqGo_406mWrCRBGCYt3Hn&ust=1609282571111000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjd9vPi8e0CFQAAAAAdAAAAABAD",
-//         "counter": 0
-//     },
-//     {
-//         "color": "blue",
-//         "character": "Captain America",
-//         "power": "shield",
-//         "cape": "no",
-//         "weather": "rain",
-//         "money": "yes",
-//         "image": "https://www.small-screen.co.uk/wp-content/uploads/2019/11/captain-america-marvel-comics-1050x450.jpg",
-//         "counter": 0
-//     },
-
-//     {
-//         "color": "grey",
-//         "character": "Black Panther",
-//         "power": "claws",
-//         "cape": "no",
-//         "weather": "sunny",
-//         "money": "yes",
-//         "image": "https://upload.wikimedia.org/wikipedia/en/9/9f/Black_Panther_OS_Vol_1_2.png",
-//         "counter": 0
-//     },
-
-//     {
-//         "color": "yellow",
-//         "character": "Wolverine",
-//         "power": "blades",
-//         "cape": "no",
-//         "weather": "Thunder",
-//         "money": "yes",
-//         "image": "https://fastly.syfy.com/sites/syfy/files/2020/02/stl149580.jpg",
-//         "counter": 0
-//     }
-
-// ]
-
-
-
-// // button for submitting data
-// let submit = document.getElementById("submit");
-
-// submit.addEventListener("click", function () {
-//     var nameElement = document.getElementById("name");
-//     var powerElement = document.getElementById("power");
-//     var capeElement = document.getElementById("cape");
-//     var weatherElement = document.getElementById("weather");
-//     var moneyElement = document.getElementById("money");
-
-//     const resultData = showImage(power, cape, weather, money);
-
-//     let output = document.getElementById("output");
-
-//     console.log(resultData);
-
-//     // display on page
-//     for (let i = 0; i < resultData.length; i++) {
-//         output.innerHTML += `<h2>${name} your superhero recomendation is ${resultData[i].character}</h2><img style="width : 100%" src="${resultData[i].image}">`;
-//     }
-// });
-
-// // function to show images
-
-// function showImage(power, cape, weather, money) {
-//     var result = []
-//     for (let i = 0; i < questions.length; i++) {
-//         if (questions[i]["power"] === power) {
-//             questions[i]["counter"]++;
-//             result.push(question[i]);
-//         }
-//         if (questions[i]["cape"] === cape) {
-//             questions[i]["counter"]++;
-//             result.push(question[i]);
-//         }
-//         if (questions[i]["weather"] === weather) {
-//             questions[i]["counter"]++;
-//             result.push(question[i]);
-//         }
-//         if (questions[i]["money"] === money) {
-//             questions[i]["counter"]++;
-//             result.push(question[i]);
-//         }
-//     }
-//         return getHighestCounter(result);
-// };
-
-// function getHighestCounter(data) {
-//     console.log(removeDuplicateObjectFromArray(data, 'character'))
-// }
-
-
-// function removeDuplicateObjectFromArray(array, key) {
-//     var check = new Set();
-//     return array.filter(obj => !check.has(obj[key]) && check.add(obj[key]));
-// }
-
 let questions = [
     {
         "color": "orange",
@@ -194,7 +46,7 @@ let questions = [
         "cape": "yes",
         "weather": "wind",
         "money": "yes",
-        "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomicvine.gamespot.com%2Fthor%2F4005-2268%2Fforums%2Fwould-you-watch-a-mighty-thor-cartoon-376272%2F&psig=AOvVaw3vqGo_406mWrCRBGCYt3Hn&ust=1609282571111000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjd9vPi8e0CFQAAAAAdAAAAABAD",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRioaFessMeo0mcS7nwD3OrVw2ry-1iXyDuhQ&usqp=CAU",
         "counter": 0
     },
     {
@@ -227,9 +79,8 @@ let questions = [
         "image": "https://fastly.syfy.com/sites/syfy/files/2020/02/stl149580.jpg",
         "counter": 0
     }
-]
-console.log(questions);
-//const resultData = showImage(power, cape, weather, money);
+];
+
 // button for submitting data
 let submit = document.getElementById("submit");
 submit.addEventListener("click", function () {
@@ -243,38 +94,40 @@ submit.addEventListener("click", function () {
     var output = document.getElementById("output");
     // display on page
     for(let i = 0; i < resultData.length; i++){
-        //console.log(resultData[i].image);
         output.innerHTML += `<h2>${name} your superhero recomendation is ${resultData[i].character}</h2><img style="width : 100%" src="${resultData[i].image}">`;
     }
-});
+
 // function to show images
-function showImage(power, cape, weather, money) {
+function showImage(power, cape, weather) { 
     var result = [];
     for (let i = 0; i < questions.length; i++) {
         if (questions[i]["power"] === power){
-            questions[i]["counter"]++
+            questions[i]["counter"]++;
             result.push(questions[i]);
         }
         if (questions[i]["cape"] === cape) {
-            questions[i]["counter"]++
+            questions[i]["counter"]++;
             result.push(questions[i]);
         }
         if (questions[i]["weather"] === weather) {
-            questions[i]["counter"]++
+            questions[i]["counter"]++;
             result.push(questions[i]);
         }
         if (questions[i]["money"] === money) {
-            questions[i]["counter"]++
+            questions[i]["counter"]++;
             result.push(questions[i]);
         }
     }
      return getHighestCounter(result);
-};
+     
+}
+
 // Gets object with highest counter
 function getHighestCounter(data) {
-    console.log(removeDuplicateObjectFromArray(data, 'character'))
+    return(removeDuplicateObjectFromArray(data, 'character'));
 }
 function removeDuplicateObjectFromArray(array, key) {
   var check = new Set();
   return array.filter(obj => !check.has(obj[key]) && check.add(obj[key]));
 }
+});
